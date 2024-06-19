f364733e36f3eb04601a35a2399ca67200524eec
bitsyinethmontes <yineth.montes@bitsamericas.com>
2024-06-19 10:00:28 -0500

    continue

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
-const { app, port } = require("./src/configurations");
-const db = require("./queries");
+const { app, port, path } = require("./src/configurations");
+//const db = require("./queries");
 
 
 app.get("/", (request, response) => {
-  response.json({ info: "node js, Express y postgres API :P " });
+  response.sendFile(path.join(__dirname, "/public/index.html"));
+  //response.json({ info: "node js, Express y postgres API :P " });
 });
 
-app.get("/users", db.getUsers);
+/*app.get("/users", db.getUsers);
 app.get("/users/:id", db.getUserById);
 app.post("/users", db.createUser);
 app.put("/users/:id", db.updateUser);
-app.delete("/users/:id", db.deleteUser);
+app.delete("/users/:id", db.deleteUser);*/
 
 app.listen(port, () => {
   console.log(`App runnin on port ${port}`);
--- src/configurations.js
+++ src/configurations.js
@@ -1,7 +1,31 @@
 const express = require("express");
+const path = require("path");
 const bodyParser = require("body-parser");
+const swaggerJsdoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+const routes = require("../routes/routes");
+
+const swaggerOptions = {
+  swaggerDefinition: {
+    openapi: "3.0.0",
+    info: {
+      title: "API Documentation",
+      version: "1.0.0",
+      description: "API Information",
+    },
+    servers: [
+      {
+        url: "http://localhost:3000",
+      },
+    ],
+  },
+  apis: ["./routes/*.js"],
+};
+const swaggerDocs = swaggerJsdoc(swaggerOptions);
+
 const app = express();
-const port = (process.env.PORT ?? 3000);
+
+const port = process.env.PORT ?? 3000;
 
 app.use(bodyParser.json());
 app.use(
@@ -10,9 +34,14 @@ app.use(
   })
 );
 
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
+app.use("/api/v1", routes);
 
 module.exports = {
   express,
   app,
   port,
+  swaggerDocs,
+  path
 }
